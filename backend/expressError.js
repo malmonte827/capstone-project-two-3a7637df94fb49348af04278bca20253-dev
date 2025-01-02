0c7c93b3e88d99482fd49f73681009522e266bbb
Michael <malmonte454@proton.me>
2025-01-02 17:00:35 -0500

    Initial commit: Added project files

--- backend/expressError.js
+++ backend/expressError.js
@@ -19,7 +19,33 @@ class NotFoundError extends ExpressError {
     }
 }
 
+/** 403 BAD REQUEST error */
+
+class ForbiddenError extends ExpressError{
+    constructor(message= "Bad Request"){
+        super(message, 403)
+    }
+}
+
+/** 401 UNAUTHORIZED error*/
+
+class UnauthorizedError extends ExpressError{
+    constructor(message = 'Unauthorized'){
+        super(message, 401)
+    }
+}
+
+/** 400 BAD REQUEST error */
+
+class BadRequestError extends ExpressError{
+    constructor(message = "Bad Request"){
+        super(message, 400)
+    }
+}
 module.exports = {
     ExpressError,
     NotFoundError,
+    ForbiddenError,
+    UnauthorizedError,
+    BadRequestError
 };
