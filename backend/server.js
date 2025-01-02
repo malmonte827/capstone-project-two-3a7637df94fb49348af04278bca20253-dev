7d67d6b9afac811b06433d75f89b862eb7ef4686
Michael <malmonte454@proton.me>
2025-01-02 17:48:21 -0500

    feat(server): Added variable for port number from config.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 "use Strict";
 
 const app = require('./app')
+const {PORT} = require("./config")
 
-app.listen(3000, function(){
-    console.log(`Started on http://localhost:3000`)
+app.listen(PORT, function(){
+    console.log(`Started on http://localhost:${PORT}`)
     
 })
