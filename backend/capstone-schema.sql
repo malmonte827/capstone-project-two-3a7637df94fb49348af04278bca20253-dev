416c90f147b79c9330ebda4b53cc386b441a7c8b
Michael <malmonte454@proton.me>
2025-01-03 18:21:11 -0500

    feat: Created tables for users & pets

---
+++ backend/capstone-schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NULL CHECK (position('@' IN email) > 1),
+    phone_number BIGINT,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE pets (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    age SMALLINT,
+    species TEXT NOT NULL,
+    hunger INTEGER NOT NULL DEFAULT 100
+);
