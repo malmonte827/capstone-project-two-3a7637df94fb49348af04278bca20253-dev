d6fd226600fae2ed5f10b717f37976b4addbef2a
Michael <malmonte454@proton.me>
2025-01-09 16:38:49 -0500

    feat: Added authenticate method

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,15 +1,44 @@
-"use strict"
+"use strict";
 
-const db = require("../db")
-const bcrypt = require("bcrypt")
-const {
-    NotFoundError,
-    BadRequestError,
-    UnauthorizedError
-} = require("../expressError")
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const {UnauthorizedError} = require("../expressError");
 
-const {BCRYPT_WORK_FACTOR} = require("../config")
 
 class User {
-    
+    /** Authenticate user using username and password
+     *
+     * Return {username, first_name, last_name, email, is_admin}
+     *
+     * If user not found or passoword is wrong throws UnauthorizedError
+     */
+
+    static async authenticate(username, password) {
+        const result = await db.query(
+            `SELECT username,
+                    password,
+                    first_name AS "firstName",
+                    last_name AS "lastName",
+                    email,
+                    is_admin AS "isAdmin"
+            FROM users
+            WHERE username = $1`,
+            [username]
+        );
+
+        const user = result.rows[0];
+
+        if (user) {
+            // compare hashed password to user password
+            const isValid = await bcrypt.compare(password, user.password);
+            if (isValid === true) {
+                delete user.password;
+                return user;
+            }
+        } else {
+            throw new UnauthorizedError("Invalid username/password");
+        }
+    }
+
 }
+ module.exports = User
