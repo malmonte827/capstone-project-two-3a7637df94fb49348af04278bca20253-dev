1a6595f3ffbd52dde83a70f46e0ce0b2f44bb19a
Michael <malmonte454@proton.me>
2025-01-10 15:52:55 -0500

    feat Added get method

--- backend/models/pet.js
+++ backend/models/pet.js
@@ -2,6 +2,7 @@
 
 const db = require("../db")
 const {NotFoundError} = require("../expressError")
+const {sqlForPartialUpdate} = require("../helpers/sql")
 
 
 class Pet {
@@ -22,5 +23,58 @@ class Pet {
         const pet = result.rows[0]
 
         return pet
+    }
+
+
+    /** Update pet data with 'data'
+     * 
+     * Returns {name, age, species, hunger}
+     * 
+     * Throws NotFoundError if not found
+     */
+    static async update(id, data){
+        const {setCols, values} = sqlForPartialUpdate(data, {})
+
+        const idIdx = `$${values.length + 1}`
+
+        const updateQuery = `UPDATE pets
+                            SET ${setCols}
+                            WHERE id = ${idIdx}
+                            RETURNING id, name, age, species, hunger, user_id AS userId`
+        
+        const result = await db.query(updateQuery, [...values, id])
+
+        const pet = result.rows[0]
+
+        if (!pet){
+            throw new NotFoundError(`No pet: ${id}`)
+        }
+
+        return pet
+    }
+
+
+    /** Get pet data
+     * 
+     * Returns {id, name, age, species, hunger, user_id}
+     * 
+     * Throws NotFoundError if not found
+     */
+    static async get(id){
+        const result = await db.query(
+            `SELECT id, name, age, species, hunger, user_id AS userId
+            FROM pets
+            WHERE id = $1`,
+            [id]
+        )
+
+        const pet = result.rows[0]
+
+        if(!pet){
+            throw new NotFoundError(`No pet: ${id}`)
+        }
+
+        return pet
+
     }
 }
