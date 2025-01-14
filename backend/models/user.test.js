85d24bec35fbc02bef977f8e0f36d3218ba28df2
Michael <malmonte454@proton.me>
2025-01-13 23:57:41 -0500

    feat: Added auth tests

--- backend/models/user.test.js
+++ backend/models/user.test.js
@@ -31,8 +31,25 @@ describe("authenticate", function () {
             firstName: "u1fn",
             lastName: "u1ln",
             email: "u1@email.com",
-            phoneNumber: '1234567890',
+            phoneNumber: "1234567890",
             isAdmin: false,
         });
+    });
+
+    test("unauth if not user", async function () {
+        try {
+            await User.authenticate("nouUser", "password");
+        } catch (err) {
+            expect(err instanceof UnauthorizedError).toBeTruthy();
+        }
+    });
+
+    test("unauth if wrong password", async function () {
+        expect.assertions(1);
+        try {
+            await User.authenticate("u1", "wrong");
+        } catch (err) {
+            expect(err instanceof UnauthorizedError).toBeTruthy();
+        }
     });
 });
