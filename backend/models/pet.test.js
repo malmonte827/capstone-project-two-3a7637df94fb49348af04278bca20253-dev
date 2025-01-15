c7f0ef14ad2eb02bb29a6ede29f6edbe8e4ca2bd
Michael <malmonte454@proton.me>
2025-01-14 23:47:17 -0500

    feat: Added create tests

--- backend/models/pet.test.js
+++ backend/models/pet.test.js
@@ -1,8 +1,8 @@
-"use strict"
+"use strict";
 
-const Pet = require("./pet")
-const db = require("../db")
-const {NotFoundError} = require("../expressError")
+const Pet = require("./pet");
+const db = require("../db");
+const { NotFoundError, BadRequestError } = require("../expressError");
 
 const {
     commonBeforeAll,
@@ -10,10 +10,30 @@ const {
     commonAfterEach,
     commonAfterAll,
     testUserIds,
+    testPetIds,
 } = require("./_testCommon");
 
 beforeAll(commonBeforeAll);
 beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
+
+/****************************************************** create */
+describe("create", function () {
+    const newPet = {
+        name: "new",
+        age: 1,
+        species: "newSpecies",
+    };
+    it("works", async function () {
+        const pet = await Pet.create({ ...newPet, user_id: testUserIds[0] });
+        expect(pet).toEqual({
+            ...newPet,
+            userId: testUserIds[0],
+            hunger: 100,
+            id: expect.any(Number),
+        });
+    });
+});
+
 
