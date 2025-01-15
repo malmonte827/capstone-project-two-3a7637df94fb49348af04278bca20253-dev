daeb37ccdd7ef6f97c5ee3ac7acc928c053b3cad
Michael <malmonte454@proton.me>
2025-01-14 21:56:08 -0500

    Initial commit

---
+++ backend/models/pet.test.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const Pet = require("./pet")
+const db = require("../db")
+const {NotFoundError} = require("../expressError")
+
+const {
+    commonBeforeAll,
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll,
+    testUserIds,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
