d926cd01728d7bdfc699a028093887765b2dcfb8
Michael <malmonte454@proton.me>
2025-01-18 15:19:29 -0500

    feat: Added PATCH /:username route to update user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,7 +6,7 @@ const User = require("../models/user");
 const jsonschema = require("jsonschema");
 const { ensureAdmin, ensureCorrectUserOrAdmin } = require("../middleware/auth");
 const userNewSchema = require("../schemas/userNew.json");
-const userRegister = require("../schemas/userRegister.json")
+const userUpdateSchema = require("../schemas/userUpdate.json")
 const { BadRequestError } = require("../expressError");
 const {createToken} = require("../helpers/token")
 
@@ -65,7 +65,7 @@ router.get("/", ensureAdmin, async function (req, res, next){
  * Authorization level: admin or same user as username
 */
 
-router.get("/:username", ensureCorrectUserOrAdmin, async function (req, rres, next) {
+router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
     try{
         const user = await User.get(req.params.username)
         return res.json({ user })
@@ -74,5 +74,26 @@ router.get("/:username", ensureCorrectUserOrAdmin, async function (req, rres, ne
     }
   })
 
+  /** PATCH /[username] {user} => {user}
+   * 
+   * Returns {username, firstName, lastName, email, phoneNumber, isAdmin }
+   * 
+   * Authorization level: admin or same user as username
+   */
+
+  router.patch("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
+    try{
+        const validator = jsonschema.validate(req.body, userUpdateSchema )
+        if(!validator.valid){
+            const errs = validator.errors.map(e => e.stack)
+            throw new BadRequestError(errs)
+        }
+
+        const user = await User.update(req.params.username, req.body)
+        return res.json({user})
+    }catch(err){
+        return next(err)
+    }
+  })
 
 module.exports = router
