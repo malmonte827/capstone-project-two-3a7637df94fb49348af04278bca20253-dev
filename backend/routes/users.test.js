5b9c4596915155d33002d4e881704c39f2038539
Michael <malmonte454@proton.me>
2025-01-18 10:46:23 -0500

    feat: Added test for GET /users

--- backend/routes/users.test.js
+++ backend/routes/users.test.js
@@ -52,7 +52,7 @@ describe("POST /users", function () {
         });
     });
 
-    test("works: admin - create admin", async  function () {
+    test("works: admin - create admin", async function () {
         const res = await request(app)
             .post("/users")
             .send({
@@ -77,7 +77,7 @@ describe("POST /users", function () {
             },
             token: expect.any(String),
         });
-    })
+    });
 
     test("unauth for users", async function () {
         const res = await request(app)
@@ -94,27 +94,25 @@ describe("POST /users", function () {
             .set("authorization", `Bearer ${u1Token}`);
         expect(res.statusCode).toEqual(401);
         expect(res.body).toEqual({
-             error: { message: 'Unauthorized', status: 401 } 
+            error: { message: "Unauthorized", status: 401 },
         });
-    })
+    });
 
     test("unauth for nonUser", async function () {
-        const res = await request(app)
-            .post("/users")
-            .send({
-                username: "newUser",
-                password: "newPassword",
-                firstName: "new",
-                lastName: "User",
-                email: "newUser@email.com",
-                phoneNumber: "1111111111",
-                isAdmin: true,
-            })
+        const res = await request(app).post("/users").send({
+            username: "newUser",
+            password: "newPassword",
+            firstName: "new",
+            lastName: "User",
+            email: "newUser@email.com",
+            phoneNumber: "1111111111",
+            isAdmin: true,
+        });
         expect(res.statusCode).toEqual(401);
         expect(res.body).toEqual({
-             error: { message: 'Unauthorized', status: 401 } 
+            error: { message: "Unauthorized", status: 401 },
         });
-    })
+    });
 
     test("bad request: missing data", async function () {
         const res = await request(app)
@@ -122,10 +120,9 @@ describe("POST /users", function () {
             .send({
                 username: "newUser",
             })
-            .set("authorization", `Bearer ${adminToken}`)
-            expect(res.statusCode).toEqual(400)
-            
-    })
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(res.statusCode).toEqual(400);
+    });
 
     test("bad request: invalid data", async function () {
         const res = await request(app)
@@ -139,7 +136,59 @@ describe("POST /users", function () {
                 phoneNumber: "1111111111",
                 isAdmin: true,
             })
-            .set("authorization", `Bearer ${adminToken}`)
+            .set("authorization", `Bearer ${adminToken}`);
         expect(res.statusCode).toEqual(400);
+    });
+});
+
+/****************************************************** GET /users */
+
+describe("GET /users", function () {
+    test("works for admins", async function () {
+        const res = await request(app)
+            .get("/users")
+            .set("authorization", `Bearer ${adminToken}`);
+            expect(res.statusCode).toEqual(200)
+        expect(res.body).toEqual({
+            users: [
+                {
+                    username: "u1",
+                    firstName: "u1fn",
+                    lastName: "u1ln",
+                    email: "u1@email.com",
+                    phoneNumber: '1111111111',
+                    isAdmin: false,
+                },
+                {
+                    username: "u2",
+                    firstName: "u2fn",
+                    lastName: "u2ln",
+                    email: "u2@email.com",
+                    phoneNumber: '2222222222',
+                    isAdmin: false,
+                },
+                {
+                    username: "u3",
+                    firstName: "u3fn",
+                    lastName: "u3ln",
+                    email: "u3@email.com",
+                    phoneNumber: '333333333',
+                    isAdmin: false,
+                },
+            ],
+        });
+    });
+
+    test("unauth for non admin user", async function () {
+        const res = await request(app)
+            .get("/users")
+            .set("authorization", `Bearer ${u1Token}`)
+            expect(res.statusCode).toEqual(401)
+    })
+
+    test("unauth for non user", async function () {
+        const res = await request(app)
+        .get("/users")
+        expect(res.statusCode).toEqual(401)
     })
 });
