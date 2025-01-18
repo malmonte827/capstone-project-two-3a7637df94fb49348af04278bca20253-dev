941b40a21172278c0d297bc2e21e7ebf4a01d940
Michael <malmonte454@proton.me>
2025-01-18 10:17:48 -0500

    feat: Added tests for POST /users route

--- backend/routes/users.test.js
+++ backend/routes/users.test.js
@@ -31,14 +31,13 @@ describe("POST /users", function () {
             .send({
                 username: "newUser",
                 password: "newPassword",
-                first_name: "new",
-                last_name: "User",
+                firstName: "new",
+                lastName: "User",
                 email: "newUser@email.com",
-                phone_number: "123-456-7890",
-                is_admin: false,
+                phoneNumber: "1111111111",
+                isAdmin: false,
             })
             .set("authorization", `Bearer ${adminToken}`);
-        console.log(res.body);
         expect(res.statusCode).toEqual(201);
         expect(res.body).toEqual({
             user: {
@@ -49,7 +48,98 @@ describe("POST /users", function () {
                 phoneNumber: "1111111111",
                 isAdmin: false,
             },
-            token: expect.any(Number),
+            token: expect.any(String),
         });
     });
+
+    test("works: admin - create admin", async  function () {
+        const res = await request(app)
+            .post("/users")
+            .send({
+                username: "newUser",
+                password: "newPassword",
+                firstName: "new",
+                lastName: "User",
+                email: "newUser@email.com",
+                phoneNumber: "1111111111",
+                isAdmin: true,
+            })
+            .set("authorization", `Bearer ${adminToken}`);
+        expect(res.statusCode).toEqual(201);
+        expect(res.body).toEqual({
+            user: {
+                username: "newUser",
+                firstName: "new",
+                lastName: "User",
+                email: "newUser@email.com",
+                phoneNumber: "1111111111",
+                isAdmin: true,
+            },
+            token: expect.any(String),
+        });
+    })
+
+    test("unauth for users", async function () {
+        const res = await request(app)
+            .post("/users")
+            .send({
+                username: "newUser",
+                password: "newPassword",
+                firstName: "new",
+                lastName: "User",
+                email: "newUser@email.com",
+                phoneNumber: "1111111111",
+                isAdmin: true,
+            })
+            .set("authorization", `Bearer ${u1Token}`);
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toEqual({
+             error: { message: 'Unauthorized', status: 401 } 
+        });
+    })
+
+    test("unauth for nonUser", async function () {
+        const res = await request(app)
+            .post("/users")
+            .send({
+                username: "newUser",
+                password: "newPassword",
+                firstName: "new",
+                lastName: "User",
+                email: "newUser@email.com",
+                phoneNumber: "1111111111",
+                isAdmin: true,
+            })
+        expect(res.statusCode).toEqual(401);
+        expect(res.body).toEqual({
+             error: { message: 'Unauthorized', status: 401 } 
+        });
+    })
+
+    test("bad request: missing data", async function () {
+        const res = await request(app)
+            .post("/users")
+            .send({
+                username: "newUser",
+            })
+            .set("authorization", `Bearer ${adminToken}`)
+            expect(res.statusCode).toEqual(400)
+            
+    })
+
+    test("bad request: invalid data", async function () {
+        const res = await request(app)
+            .post("/users")
+            .send({
+                username: "newUser",
+                password: "newPassword",
+                firstName: "new",
+                lastName: "User",
+                email: "not an email",
+                phoneNumber: "1111111111",
+                isAdmin: true,
+            })
+            .set("authorization", `Bearer ${adminToken}`)
+        expect(res.statusCode).toEqual(400);
+    })
 });
