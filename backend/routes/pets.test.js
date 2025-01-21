daff37f7c8cbbdaa392db0a8190e2cfc6466af77
Michael <malmonte454@proton.me>
2025-01-21 12:26:11 -0500

    test: Added POST /

--- backend/routes/pets.test.js
+++ backend/routes/pets.test.js
@@ -3,7 +3,6 @@
 const app = require("../app");
 
 const request = require("supertest");
-const r = `/users/:user_id`
 
 const {
     commonBeforeAll,
@@ -11,6 +10,9 @@ const {
     commonAfterEach,
     commonAfterAll,
     testUserIds,
+    u1Token,
+    u2Token,
+    adminToken,
 } = require("./_testCommon");
 
 beforeAll(commonBeforeAll);
@@ -21,13 +23,39 @@ afterAll(commonAfterAll);
 /****************************************************** POST /pets */
 
 describe("POST /pets", function () {
-    test("works", async function () {
-        const res = await request(app).post(`/users/u1/pets`).send({
-            name: "newPet",
-            age: 2,
-            species: "pet",
-            user_id: testUserIds[0],
+    test("works: admin", async function () {
+        const res = await request(app)
+            .post(`/users/u1/pets`)
+            .set("authorization", `Bearer ${adminToken}`)
+            .send({
+                name: "newPet",
+                age: 2,
+                species: "pet",
+                user_id: testUserIds[1],
+            });
+        expect(res.body).toEqual({
+            pet: {
+                id: expect.any(Number),
+                name: "newPet",
+                age: 2,
+                species: "pet",
+                hunger: 100,
+                userId: testUserIds[1],
+            },
         });
+        expect(res.statusCode).toEqual(201);
+    });
+
+    test("works: same user", async function () {
+        const res = await request(app)
+            .post(`/users/u1/pets`)
+            .set("authorization", `Bearer ${u1Token}`)
+            .send({
+                name: "newPet",
+                age: 2,
+                species: "pet",
+                user_id: testUserIds[0],
+            });
         expect(res.body).toEqual({
             pet: {
                 id: expect.any(Number),
@@ -37,26 +65,53 @@ describe("POST /pets", function () {
                 hunger: 100,
                 userId: testUserIds[0],
             },
-        })
-        expect(res.statusCode).toEqual(201)
+        });
+        expect(res.statusCode).toEqual(201);
     });
 
-    // test("bad request: missing data", async function () {
-    //     const res = await request(app)
-    //     .post("/pets")
-    //     .send({
-    //         name: "newPet"
-    //     });
-    //     expect(res.statusCode).toEqual(400)
-    // })
+    test("unauth: different user", async function () {
+        const res = await request(app)
+            .post(`/users/u1/pets`)
+            .set("authorization", `Bearer ${u2Token}`)
+            .send({
+                name: "newPet",
+                age: 2,
+                species: "pet",
+                user_id: testUserIds[0],
+            });
+        expect(res.statusCode).toEqual(401);
+    });
 
-    // test("bad request: invalid data", async function () {
-    //     const res = await request(app).post("/pets").send({
-    //         name: "newPet",
-    //         species: "pet",
-    //         age:"not a number",
-    //         user_id: testUserIds[0],
-    //     });
-    //     expect(res.statusCode).toEqual(400)
-    // })
+    test("unauth: non user", async function () {
+        const res = await request(app).post(`/users/u1/pets`).send({
+            name: "newPet",
+            age: 2,
+            species: "pet",
+            user_id: testUserIds[0],
+        });
+        expect(res.statusCode).toEqual(401);
+    });
+
+    test("bad request: missing data", async function () {
+        const res = await request(app)
+            .post("/users/u1/pets")
+            .set("authorization", `Bearer ${adminToken}`)
+            .send({
+                name: "newPet",
+            });
+        expect(res.statusCode).toEqual(400);
+    });
+
+    test("bad request: invalid data", async function () {
+        const res = await request(app)
+            .post("/users/u1/pets")
+            .set("authorization", `Bearer ${adminToken}`)
+            .send({
+                name: "newPet",
+                species: "pet",
+                age: "not a number",
+                user_id: testUserIds[0],
+            });
+        expect(res.statusCode).toEqual(400);
+    });
 });
