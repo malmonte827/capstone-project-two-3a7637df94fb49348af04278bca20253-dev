8188f804cf31280381a550592a6b4e42accbc21c
Michael <malmonte454@proton.me>
2025-01-21 13:53:00 -0500

    feat: Added GET /:id to get pet by id

--- backend/routes/pets.js
+++ backend/routes/pets.js
@@ -1,43 +1,52 @@
-"use strict"
+"use strict";
 
-const express = require("express")
-const router = express.Router({mergeParams: true})
-const Pet = require("../models/pet")
-const {ensureCorrectUserOrAdmin} = require("../middleware/auth")
-const jsonschema = require("jsonschema")
-const petNewSchema = require("../schemas/petNew.json")
-const {BadRequestError} = require("../expressError")
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const Pet = require("../models/pet");
+const { ensureCorrectUserOrAdmin } = require("../middleware/auth");
+const jsonschema = require("jsonschema");
+const petNewSchema = require("../schemas/petNew.json");
+const { BadRequestError } = require("../expressError");
 /** Routes for pets */
 
 /** POST / {pet} => {pet}
- * 
+ *
  * Returns {pet: [{id, name, age, species, hunger, user_id}, ...]
- * 
+ *
  * Authorization level: Admin or same as user_id
  */
 
 router.post("/", ensureCorrectUserOrAdmin, async function (req, res, next) {
-    try{
-        const validator = jsonschema.validate(req.body, petNewSchema)
-        if(!validator.valid){
-            const errs = validator.errors.map(e => e.stack)
-            throw new BadRequestError(errs)
+    try {
+        const validator = jsonschema.validate(req.body, petNewSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map((e) => e.stack);
+            throw new BadRequestError(errs);
         }
 
-        const pet = await Pet.create(req.body)
-        return res.status(201).json({pet})
-    }catch(err){
-        return next(err)
+        const pet = await Pet.create(req.body);
+        return res.status(201).json({ pet });
+    } catch (err) {
+        return next(err);
     }
-})
+});
 
-router.get("/", ensureCorrectUserOrAdmin, async function(req, res, next) {
-    try{
-        const pets = await Pet.getAll(req.params.username)
-        return res.status(200).json({pets})
-    }catch(err){
-        return next(err)
+router.get("/", ensureCorrectUserOrAdmin, async function (req, res, next) {
+    try {
+        const pets = await Pet.getAll(req.params.username);
+        return res.status(200).json({ pets });
+    } catch (err) {
+        return next(err);
     }
-})
+});
 
-module.exports = router
+router.get("/:id", ensureCorrectUserOrAdmin, async function (req, res, next) {
+    try {
+        const pet = await Pet.get(req.params.id);
+        return res.status(200).json({ pet });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;
