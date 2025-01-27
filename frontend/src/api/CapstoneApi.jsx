c4732131d73b7a48dc176974c559816f258d4e35
Michael <malmonte454@proton.me>
2025-01-26 21:08:00 -0500

    Initial commit: feat: Added api to send request to backend

---
+++ frontend/src/api/CapstoneApi.jsx
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+const BASE_URL = process.env.React_APP_BASE_URL || "http://localhost:3001"
+
+/** APi class
+ * 
+ * Staic class to get/send to the API
+ */
+
+class CapstoneApi{
+    // token for interacting with the API
+    static token
+
+    static async request(endpoint, data={}, method="get"){
+        console.debug(`API Call:`, endpoint, data, method)
+
+        const url = `${BASE_URL}/${endpoint}`
+        const headers = {Authorization: `Bearer ${CapstoneApi.token}`}
+        const params = (method === "get") ? data : {}
+
+        try{
+            return (await axios({url, method, data, params,headers})).data
+        }catch(err){
+            console.error(`API Error:`, err.response)
+            let message = err.response.data.error.message
+            throw Array.isArray(message) ? message : [message]
+        }
+    }
+}
