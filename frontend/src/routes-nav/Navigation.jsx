3a5f60b67062706f6bca533d6585b8c0f3cc3e86
Michael <malmonte454@proton.me>
2025-01-27 21:26:57 -0500

    Initial commit: Added Navbar

---
+++ frontend/src/routes-nav/Navigation.jsx
@@ -0,0 +1,62 @@
+import React, { useContext } from "react";
+import UserContext from "../auth/UserContext";
+import { Link, NavLink } from "react-router-dom";
+
+/** Navbar for site
+ *
+ * Shows on every page
+ *
+ * when a user is logged in it shows links to other areas of site
+ * when not shows links to signup and login forms
+ *
+ */
+function Navigation({ logout }) {
+    const { currentUser } = useContext(UserContext);
+
+    function loggedInNav() {
+        return (
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item me-4">
+                    <NavLink className="nav-link" to="/">
+                        Home
+                    </NavLink>
+                </li>
+                <li className="nav-item me-4">
+                    <NavLink className="nav-link" to="/" onClick={logout}>
+                        Logout {currentUser.firstName || currentUser.username}
+                    </NavLink>
+                </li>
+            </ul>
+        );
+    }
+
+    function loggedOutNav() {
+        return (
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item me-4">
+                    <NavLink className="nav-link" to="/login">
+                        Login
+                    </NavLink>
+                </li>
+                <li className="nav-item me-4">
+                    <NavLink className="nav-link" to="/signup">
+                        Signup
+                    </NavLink>
+                </li>
+            </ul>
+        );
+    }
+
+    return (
+        <nav className="Navigation navbar navbar-expandd-md">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    Capstone
+                </Link>
+                {currentUser ? loggedInNav() : loggedOutNav()}
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
