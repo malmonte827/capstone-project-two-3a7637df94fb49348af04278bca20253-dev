57e32aaedb0faba6bab6d630682511f33a0ebf84
Michael <malmonte454@proton.me>
2025-01-27 23:39:43 -0500

    fix: syntax error

--- frontend/src/common/Alert.jsx
+++ frontend/src/common/Alert.jsx
@@ -7,7 +7,7 @@ import React from "react";
 
 function Alert({ type = "danger", messages = [] }) {
     return (
-        <div class={`alert alert-${type}`} role="alert">
+        <div className={`alert alert-${type}`} role="alert">
             {messages.map((error) => (
                 <p className="mb-0 small" key={error}>
                     {error}
