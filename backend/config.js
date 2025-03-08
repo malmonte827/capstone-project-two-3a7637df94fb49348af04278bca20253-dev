ed47cd7dd8e7bdcfe6017ef5d56cc5448705e41c
Michael <malmonte454@proton.me>
2025-03-08 14:40:28 -0500

    fix: removed hardcoded databse urls & secret keys

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,7 @@
 require("dotenv").config()
 require("colors");
 
-const SECRET_KEY = process.env.SECRET_KEY || "secet-dev"
+const SECRET_KEY = process.env.SECRET_KEY
 
 const PORT = +process.env.PORT || 3001
 
@@ -22,10 +22,8 @@ function getDatabaseUri(){
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
 
 console.log("Capstone Config:".green)
-console.log("SECRET_KEY:".yellow, SECRET_KEY)
 console.log("PORT:".yellow, PORT.toString())
 console.log("BCRYPT_WORK_FACTOR:".yellow, BCRYPT_WORK_FACTOR)
-console.log("Database:".yellow, getDatabaseUri())
 console.log("-----------")
 
 module.exports = {
