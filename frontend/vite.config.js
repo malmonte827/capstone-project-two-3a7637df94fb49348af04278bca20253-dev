22b3d38487b98e2a1d0e774c97d9b0414c02389c
Michael <malmonte454@proton.me>
2025-03-14 22:12:02 -0400

    Initial commit: Config for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // Makes it easier to use `describe`, `it`, etc. globally
+    environment: 'jsdom', // Simulate a browser environment
+}})
